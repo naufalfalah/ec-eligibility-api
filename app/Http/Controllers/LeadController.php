0e9eea22aefdc6bfa1ed66806fe85f7a2efb0637
Naufal Falah <naufalfallah@gmail.com>
2025-05-01 14:48:12 +0700

    chore: scribe

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -8,6 +8,36 @@ use Illuminate\Support\Facades\DB;
 
 class LeadController extends Controller
 {
+    /**
+     * Get all leads.
+     *
+     * Retrieves a list of all leads in the system, sorted by the latest created.
+     *
+     * @group Leads
+     * @response {
+     *   "success": true,
+     *   "data": [
+     *     {
+     *       "id": 1,
+     *       "household": "Example",
+     *       "citizenship": "Singapore Citizen",
+     *       "requirement": "Yes",
+     *       "household_income": "3000",
+     *       "ownership_status": "No, do not own any HDB",
+     *       "private_property_ownership": "No",
+     *       "first_time_applicant": "Yes",
+     *       "name": "John Doe",
+     *       "email": "johndoe@example.com",
+     *       "phone_number": "123456789",
+     *       "verified_at": "2025-05-01T07:00:00",
+     *       "send_discord": false,
+     *       "created_at": "2025-05-01T07:00:00",
+     *       "updated_at": "2025-05-01T07:00:00",
+     *       "deleted_at": null
+     *     }
+     *   ]
+     * }
+     */
     public function index()
     {
         $leads = Lead::latest()->get();
@@ -18,6 +48,42 @@ class LeadController extends Controller
         ]);
     }
 
+    /**
+     * Create a new lead.
+     *
+     * Stores a new lead in the database.
+     *
+     * @group Leads
+     * @bodyParam household string required The household type of the lead. Example: Example Household
+     * @bodyParam citizenship string required The citizenship status. Example: Singapore Citizen
+     * @bodyParam requirement string required The requirement status. Example: Yes
+     * @bodyParam household_income string required The household income. Example: 3000
+     * @bodyParam ownership_status string required The ownership status. Example: No, do not own any HDB
+     * @bodyParam private_property_ownership string required The private property ownership status. Example: No
+     * @bodyParam first_time_applicant string required First time applicant status. Example: Yes
+     * @bodyParam name string required The name of the lead. Example: John Doe
+     * @bodyParam email string required The email address of the lead. Example: johndoe@example.com
+     * @bodyParam phone_number string required The phone number of the lead. Example: 123456789
+     * @response {
+     *   "status": "success",
+     *   "message": "Form submitted successfully",
+     *   "data": {
+     *     "lead_id": 1,
+     *     "result": "congratulation"
+     *   },
+     *   "listing": {
+     *     "result": "singmap-congratulation"
+     *   }
+     * }
+     * @response 422 {
+     *   "message": "The given data was invalid.",
+     *   "errors": {
+     *     "household": [
+     *       "The household field is required."
+     *     ]
+     *   }
+     * }
+     */
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -42,6 +108,7 @@ class LeadController extends Controller
                 'ownership_status' => $validated['ownership_status'],
                 'private_property_ownership' => $validated['private_property_ownership'],
                 'first_time_applicant' => $validated['first_time_applicant'],
+                'name' => $validated['name'],
                 'phone_number' => $validated['phone_number'],
                 'email' => $validated['email'],
             ]);
@@ -60,23 +127,23 @@ class LeadController extends Controller
                     $lead->household_income === 'No' ||
                     $lead->private_property_ownership === 'Yes':
                     $response['data']['result'] = 'disqualification';
-                    $response['listing']['result'] = 'singmap-appeal-mop';
+                    $response['data']['listing'] = 'singmap-appeal-mop';
                     break;
                 case $lead->ownership_status === 'Yes, MOP completed':
                     $response['data']['result'] = 'congratulation';
-                    $response['listing']['result'] = 'singmap-congratulation';
+                    $response['data']['listing'] = 'singmap-congratulation';
                     break;
                 case $lead->ownership_status === 'Yes, still within MOP':
                     $response['data']['result'] = 'mop';
-                    $response['listing']['result'] = 'singmap-appeal-mop';
+                    $response['data']['listing'] = 'singmap-appeal-mop';
                     break;
                 case $lead->ownership_status === 'No, do not own any HDB':
                     $response['data']['result'] = 'appeal';
-                    $response['listing']['result'] = 'singmap-appeal-mop';
+                    $response['data']['listing'] = 'singmap-appeal-mop';
                     break;
                 default:
                     $response['data']['result'] = 'disqualification';
-                    $response['listing']['result'] = 'singmap-appeal-mop';
+                    $response['data']['listing'] = 'singmap-appeal-mop';
                     break;
             }
 
@@ -91,6 +158,39 @@ class LeadController extends Controller
         }
     }
 
+    /**
+     * Get a single lead.
+     *
+     * Retrieves details of the lead with the specified ID.
+     *
+     * @group Leads
+     * @urlParam id integer required The ID of the lead. Example: 1
+     * @response {
+     *   "success": true,
+     *   "data": {
+     *     "id": 1,
+     *     "household": "Example",
+     *     "citizenship": "Singapore Citizen",
+     *     "requirement": "Yes",
+     *     "household_income": "3000",
+     *     "ownership_status": "No, do not own any HDB",
+     *     "private_property_ownership": "No",
+     *     "first_time_applicant": "Yes",
+     *     "name": "John Doe",
+     *     "email": "johndoe@example.com",
+     *     "phone_number": "123456789",
+     *     "verified_at": "2025-05-01T07:00:00",
+     *     "send_discord": false,
+     *     "created_at": "2025-05-01T07:00:00",
+     *     "updated_at": "2025-05-01T07:00:00",
+     *     "deleted_at": null
+     *   }
+     * }
+     * @response 404 {
+     *   "success": false,
+     *   "message": "Lead not found."
+     * }
+     */
     public function show($id)
     {
         try {
